3f1dd3dc749f7ae0519159db715b7fe5d41b2312
lbaitemple <lbai@temple.edu>
2020-07-04 09:58:36 -0400

    Update teach.sh

--- ifttt/teach.sh
+++ ifttt/teach.sh
@@ -3,6 +3,8 @@ model="/home/ftp/storage/model/keras_model.h5"
 label="/home/ftp/storage/model/labels.txt"
 camerafolder="/home/ftp/sdcard/Camera1/"
 ifttt_link="https://maker.ifttt.com/trigger/MotionCam/with/key/ddMmb_mQAvNb2lKKf0tHz8IC1s4NE_8LWTJe5iIf6h-"
+user="lbai"
+passwd="test"
 
 
 #get the latest image [DO NOT CHANGE ANYTHING BEBLOW
@@ -12,7 +14,7 @@ ifile="${directory}/${imgfile}"
 #echo $ifile
 
 # get teachable machine predication 
-pred=`curl -s -u lbai:test -F "picture=@$ifile" -F "model=@$model"  -F "class=@$label" -F "format=text" http://templebits.duckdns.org/classfy`
+pred=`curl -s -u $user:$passwd -F "picture=@$ifile" -F "model=@$model"  -F "class=@$label" -F "format=text" http://templebits.duckdns.org/classfy`
 
 sendcmd='{"value1": "'$pred'"}'
 curl -s -X POST -H "Content-Type: application/json" -d "$sendcmd" $ifttt_link
